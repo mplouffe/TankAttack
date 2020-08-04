41f8dab2cf31740ec7e58e44cad6cc2c5cc43f86
mplouffe <matheuplouffe@gmail.com>
2020-08-03 20:41:24 -0500

    Cleaning up project.

---
+++ Assets/lvl0/Scripts/ECS/ComponentData/MoveablePlayerComponent.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.NetCode;
+
+[GenerateAuthoringComponent]
+public struct MovablePlayerComponent : IComponentData
+{
+    [GhostDefaultField]
+    public int PlayerId;
+}
